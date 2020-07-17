02e3f8722e32f72e450c0a542f10191e9e2cf851
Andrew Kennedy <akennedycode@gmail.com>
2020-07-17 00:28:28 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -7,14 +7,12 @@ function callMe() {
   return lyric;
 }
 
-
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
